f8c36a2fa146e154198c81de29bd5406f4bcf05d
varshithayempally <varshithayempally@gmail.com>
2025-01-30 21:25:04 +0530

    Add files via upload

---
+++ Conditions.java
@@ -0,0 +1,13 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int num=-20;
+        if(num>0){
+            System.out.println("the number is positive.");
+        }else if(num<0){
+            System.out.println("the number is negative.");
+        }else{
+            System.out.println("the number is zero.");
+        }
+    }
+    
+}
